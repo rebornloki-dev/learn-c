64135fd508fd179291a6bb8c4e017f20eafcfe98
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-01 21:41:10 +0100

    feat: add initial AxonBoot program with welcome message

---
+++ AxonBoot.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+ int main(){
+    printf("================================================\n");
+    printf("          Welcome to Project Axon\n");
+    printf("          Version 0.0.1\n");
+    printf("          April 1st 2026\n");
+    printf("================================================\n");
+    return  0 ;
+}
