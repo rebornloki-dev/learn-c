8cbb8aa35bc6af0908260193e48fac1b9ad27104
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-01 22:44:42 +0100

    feat: update pizza message formatting in HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,7 +4,10 @@ int main (){
     //This is a comment blah blah
     /*comment multi line*/
 
-    printf( "I like Cheeze Pizza\n");
+    printf( "I \nlike\n Cheeze\n Pizza\n");// \n is for newline
+    
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");//\t means tab which evenly spaces text
+    printf("\"I like pizza\"-Abraham Lincoln probably");// In order to add quotes you can do "\"
     printf("Its really good\n");
     return 0;
 }
