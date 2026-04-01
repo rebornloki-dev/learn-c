8d61f71d5ea03cb5fe1695a6d113a907d8e44ba2
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-01 22:52:39 +0100

    feat: add quote to welcome message in AxonBoot.C

--- AxonBoot.C
+++ AxonBoot.C
@@ -5,6 +5,7 @@
     printf("          Welcome to Project Axon\n");
     printf("          Version 0.0.1\n");
     printf("          April 1st 2026\n");
+    printf("\"I like pizza\"-Abraham Lincoln probably\n");
     printf("================================================\n");
     return  0 ;
 }
