b784fb3dfee2fdda29fad53c36d9ea7004861c03
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-02 14:07:27 +0100

    feat: add introductory program in ahem.c

---
+++ Introductions/Exercises/1a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("========================================\n");
+    printf("           Learn about me!\n");
+    printf("            I like trains\n");
+    printf("              I love da burgers\n");
+    printf("                 Im learning C to create my own OS\n ");
+    printf("========================================\n");
+    return 0;
+}
