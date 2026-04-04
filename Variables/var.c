f756b9865ba384324f324aa902d630461bc1cc09
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-04 07:53:35 +0100

    fix: improve comments for variable storage explanation in var.c

--- Variables/var.c
+++ Variables/var.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    /*variable = Allocated Space in memory to store a value.
+    /*variable = Allocated Space in memory to store a value. 
                 We refer to a variable's name to access stored values.
+                That variable now behaves as if it was the value it contains
+                BUT we need to declare what type of data we are storing 
+    */
+    
+    return 0;
 
 }
