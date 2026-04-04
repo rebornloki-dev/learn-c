5bb963f7e44b516f6dfc07224bf080646d11784d
rebornloki-dev <lydieakokpe431@gmail.com>
2026-04-04 07:40:05 +0100

    added many things

---
+++ Introductions/Escape Sequences/escape-seqs.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ int main(){
+    /* Escape Sequence = character combinations consisting of a backslash \
+                         followed by a lettter or combination of digits.
+                        They specify actions within a line or string of text.
+                        \n =newline
+                        \t = tab
+    */
+     printf("I like pizza\n");
+    return 0;
+    }
--- Introductions/Print Statements/HelloWorld.c
+++ Introductions/Print Statements/HelloWorld.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main (){
-    //This is a comment blah blah
-    /*comment multi line*/
-
-    printf( "I \nlike\n Cheeze\n Pizza\n");// \n is for newline
-    
-    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");//\t means tab which evenly spaces text
-    printf("\"I like pizza\"-Abraham Lincoln probably\n");// In order to add quotes you can do "\"
+    //This is a comment 
+    /*THIS
+    IS
+    A
+    MULTILINE
+    COMMENT*/
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I like pizza\"-Abraham Lincoln probably\n");
     printf("Its really good\n");
     return 0;
 }
